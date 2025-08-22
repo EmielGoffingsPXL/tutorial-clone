ca88073f5eabb541d9b0c84bad01eacd175c0c49
Wim Bervoets <wim.bervoets@pxl.be>
2025-08-22 21:37:50 +0200

    added console application

---
+++ ClonedProject/ClonedProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClonedProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Proficiat, je hebt je eerste clone gemaakt!");
+        }
+    }
+}
